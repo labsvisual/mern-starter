83494166692e62c2d9c02e00c9b9ec1b97ee3704
Shreyansh Pandey <334@doonschool.com>
2016-02-18 18:52:32 +0530

    minor fix

--- shared/components/Footer/Footer.js
+++ shared/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer(props, context) {
   return (
    <div className="footer">
    			<p>&copy; 2016 &middot; Hashnode &middot; LinearBytes Inc.</p>
-   			<p>We are on Twitter : <a href="https://twitter.com/@mern_io" target="_Blank">@mern_io</a></p>
+   			<p>We are on Twitter : <a href="https://twitter.com/@mern_io" target="_blank">@mern_io</a></p>
    </div>
  );
 }
